4958755362f0163d56c8fc1cc53239ff2297c0e8
rmata <rolandomata@hotmail.com>
2022-01-18 15:34:10 -0600

    Tarea 2, Rolando Mata

--- MobileApp/MobileApp/App.cs
+++ MobileApp/MobileApp/App.cs
@@ -1,13 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 using Xamarin.Forms;
+    
 
 namespace MobileApp
 {
-	public partial class App : Application
-	{
-		public App()
-		{
-			MainPage = new ContentPage { Content = new Label { Text = "Hello, World!", VerticalOptions = LayoutOptions.Center, HorizontalOptions = LayoutOptions.Center } };
-		}
-	}
+
+    public partial class App : Application { 
+
+
+public App()
+    {
+        MainPage = new ContentPage
+        {
+            Content = BuildCalculator(),
+            BackgroundColor = Color.Blue,
+        };
+    }
+    private View BuildCalculator()
+    {
+        var entryField = new Xamarin.Forms.Entry()
+        {
+            Text = "123"
+        };
+        entryField.IsReadOnly = true; var container = new StackLayout();
+        container.Orientation = StackOrientation.Vertical;
+        container.BackgroundColor = Color.AntiqueWhite; container.Children.Add(new StackLayout
+        {
+            Orientation = StackOrientation.Horizontal,
+            Children =
+{
+new Button { Text = "7" },
+new Button { Text = "8" },
+new Button { Text = "9" },
 }
+        }
+        ); container.Children.Add(new StackLayout
+        {
+            Orientation = StackOrientation.Horizontal,
+            Children =
+        {
+new Button { Text = "4" },
+new Button { Text = "5" },
+new Button { Text = "6" },
+        }
+        }
+        );
+        container.Children.Add(new StackLayout
+        {
+            Orientation = StackOrientation.Horizontal,
+            Children =
+{
+new Button { Text = "1" },
+new Button { Text = "2" },
+new Button { Text = "3" },
+}
+        }
+        );
+        return new StackLayout
+        {
+            Orientation = StackOrientation.Vertical,
+            VerticalOptions = LayoutOptions.Center,
+            HorizontalOptions = LayoutOptions.Center,
+            Children =
+{
+entryField,
+container,
+}
+        }; //return entryField;
+    }
+}
+}
+
+    
+
